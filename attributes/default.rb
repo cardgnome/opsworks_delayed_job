169a6dd8ceffdb2ac20665db04e1e3c299a632e8
Chad McGimpsey <chad.mcgimpsey@gmail.com>
2018-05-22 10:29:56 -0600

    added c3 large instances

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,31 +1,31 @@
-include_attribute "deploy"
+include_attribute 'deploy'
 
 default[:delayed_job] = {}
 default[:delayed_job][:pool_size] =
   case node[:opsworks][:instance][:instance_type]
-	when 'm1.small' then 1
-	when 'c1.medium' then 2
-  when 'm2.medium' then 2
-	when 'c1.xlarge' then 8
-	when 'm1.large' then 4
+  when 'm3.medium' then 2
   when 'm3.large' then 4
-	else 
-	  2
+  when 'c3.large' then 4
+  when 'c3.xlarge' then 8
+  when 'c3.2xlarge' then 16
+  when 'c3.4xlarge' then 32
+  when 'c3.8xlarge' then 64
+  else
+    2
   end
 
 node[:deploy].each do |application, deploy|
-  
   default[:delayed_job][application] = {}
   default[:delayed_job][application][:pools] = {}
-  
-  # Default to node[:delayed_job][:pool_size] workers, each with empty ({}) config.
-  default[:delayed_job][application][:pools][:default] = Hash[node[:delayed_job][:pool_size].times.map{|i| [i.to_s, {}] }]
-  
-  # Use node[:delayed_job][application][:pools][HOSTNAME] if provided.
-  default[:delayed_job][application][:pool] = node[:delayed_job][application][:pools][node[:opsworks][:instance][:hostname]] || node[:delayed_job][application][:pools][:default]
-  Chef::Log.debug("Set delayed_job attributes for #{application} to #{node[:delayed_job][application].to_hash.inspect}")
-  
-  default[:delayed_job][application][:restart_command] = "sudo monit restart -g delayed_job_#{application}_group"
-  
-end
 
+  # Default to node[:delayed_job][:pool_size] workers, each with empty ({}) config.
+  default[:delayed_job][application][:pools][:default] =
+    Hash[node[:delayed_job][:pool_size].times.map { |i| [i.to_s, {}] }]
+
+  # Use node[:delayed_job][application][:pools][HOSTNAME] if provided.
+  default[:delayed_job][application][:pool] =
+    node[:delayed_job][application][:pools][node[:opsworks][:instance][:hostname]] ||
+    node[:delayed_job][application][:pools][:default]
+
+  default[:delayed_job][application][:restart_command] = "sudo monit restart -g delayed_job_#{application}_group"
+end
