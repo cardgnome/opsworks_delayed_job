61f66759e12753b25340cfe32c127d4c497dc1ab
Chad McGimpsey <chad.mcgimpsey@gmail.com>
2018-05-23 09:54:27 -0600

    made tmp folder for delayed_job

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -4,7 +4,6 @@ include_recipe "delayed_job::service"
 
 # setup delayed_job service per app
 node[:deploy].each do |application, deploy|
-  
   if deploy[:application_type] != 'rails'
     Chef::Log.debug("Skipping delayed_job::setup application #{application} as it is not a Rails app")
     next
@@ -19,20 +18,26 @@ node[:deploy].each do |application, deploy|
   #   group deploy[:group]
   #   path deploy[:deploy_to]
   # end
-  
+
+  directory "/srv/www/tmp" do
+    mode 0755
+    owner deploy[:user]
+    group deploy[:group]
+    action :create
+  end
+
   # Allow deploy user to restart workers
   template "/etc/sudoers.d/#{deploy[:user]}" do
     mode 0440
     source "sudoer.erb"
     variables :user => deploy[:user]
   end
-  
+
   template "/etc/monit.d/delayed_job_#{application}.monitrc" do
     mode 0644
     source "delayed_job.monitrc.erb"
     variables(:deploy => deploy, :application => application, :delayed_job => node[:delayed_job][application])
-    
+
     notifies :reload, resources(:service => "monit"), :immediately
   end
-  
 end
